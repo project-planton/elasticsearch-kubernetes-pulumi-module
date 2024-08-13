735daa1580721287f17abe30e2d912b8e52dfbbd
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-13 15:58:50 +0530

    refactored for protos in one package per api resource

--- pkg/main.go
+++ pkg/main.go
@@ -2,13 +2,13 @@ package pkg
 
 import (
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/elasticsearchkubernetes/model"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/elasticsearchkubernetes"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type ResourceStack struct {
-	Input  *model.ElasticsearchKubernetesStackInput
+	Input  *elasticsearchkubernetes.ElasticsearchKubernetesStackInput
 	Labels map[string]string
 }
 
